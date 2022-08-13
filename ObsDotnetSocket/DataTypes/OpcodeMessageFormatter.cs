2924d052409e5f69d9f2355cdeb0c665a5251840
jeiea <solum5013@gmail.com>
2022-08-14 02:24:28 +0900

    refactor: reduce lines

--- ObsDotnetSocket/DataTypes/OpcodeMessageFormatter.cs
+++ ObsDotnetSocket/DataTypes/OpcodeMessageFormatter.cs
@@ -4,6 +4,10 @@ namespace ObsDotnetSocket.DataTypes {
   using MessagePack.Resolvers;
   using System;
 
+  class UnexpectedProtocolException : Exception {
+    public UnexpectedProtocolException(string? message = null) : base(message ?? "It's not seem to be obs websocket message") { }
+  }
+
   class OpcodeMessageFormatter : IMessagePackFormatter<IOpcodeMessage> {
     private static readonly DynamicObjectResolver _resolver = DynamicObjectResolver.Instance;
 
@@ -11,20 +15,20 @@ namespace ObsDotnetSocket.DataTypes {
     private readonly IMessagePackFormatter<Identify> _identifyFormatter = _resolver.GetFormatter<Identify>();
     private readonly IMessagePackFormatter<Identified> _identifiedFormatter = _resolver.GetFormatter<Identified>();
     private readonly IMessagePackFormatter<Reidentify> _reidentifyFormatter = _resolver.GetFormatter<Reidentify>();
-    private readonly IMessagePackFormatter<Event> _eventFormatter = _resolver.GetFormatter<Event>();
+    private readonly IMessagePackFormatter<Event<object?>> _eventFormatter = _resolver.GetFormatter<Event<object?>>();
     private readonly IMessagePackFormatter<Request> _requestFormatter = _resolver.GetFormatter<Request>();
     private readonly IMessagePackFormatter<RequestResponse> _requestResponseFormatter = _resolver.GetFormatter<RequestResponse>();
 
     public IOpcodeMessage Deserialize(ref MessagePackReader reader, MessagePackSerializerOptions options) {
       int propertyCount = reader.ReadMapHeader();
       if (propertyCount != 2) {
-        throw new Exception("It's not seem to be obs websocket message");
+        throw new UnexpectedProtocolException();
       }
 
-      int opcode = PeekOp(ref reader, options);
+      int opcode = FormatterUtil.SeekByKey(reader, "op").ReadInt32();
       string key = reader.ReadString();
       if (key != "d") {
-        throw new Exception("It's not seem to be obs websocket message");
+        throw new UnexpectedProtocolException();
       }
 
       IOpcodeMessage data = opcode switch {
@@ -45,49 +49,26 @@ namespace ObsDotnetSocket.DataTypes {
       return data;
     }
 
-    private static int PeekOp(ref MessagePackReader reader, MessagePackSerializerOptions options) {
-      var peeker = reader.CreatePeekReader();
-      while (true) {
-        string key = peeker.ReadString();
-        if (key == "op") {
-          return peeker.ReadUInt16();
-        }
-        peeker.Skip();
-      }
-    }
-
     public void Serialize(ref MessagePackWriter writer, IOpcodeMessage value, MessagePackSerializerOptions options) {
       writer.WriteMapHeader(2);
       writer.Write("op");
       writer.Write((int)value.Op);
       writer.Write("d");
 
-      switch (value) {
-      case Hello data:
-        _helloFormatter.Serialize(ref writer, data, options);
-        break;
-      case Identify data:
-        _identifyFormatter.Serialize(ref writer, data, options);
-        break;
-      case Identified data:
-        _identifiedFormatter.Serialize(ref writer, data, options);
-        break;
-      case Reidentify data:
-        _reidentifyFormatter.Serialize(ref writer, data, options);
-        break;
-      case Event data:
-        _eventFormatter.Serialize(ref writer, data, options);
-        break;
-      case Request data:
-        _requestFormatter.Serialize(ref writer, data, options);
-        break;
-      case RequestResponse data:
-        _requestResponseFormatter.Serialize(ref writer, data, options);
-        break;
-      default:
-        StandardResolver.Instance.GetFormatter<object>().Serialize(ref writer, value, options);
-        break;
-      };
+      MessagePackSerializer.Serialize(value.GetType(), ref writer, value, options);
+    }
+  }
+
+  internal static class FormatterUtil {
+    public static MessagePackReader SeekByKey(MessagePackReader reader, string key) {
+      var peeker = reader.CreatePeekReader();
+      while (true) {
+        string cursor = peeker.ReadString();
+        if (cursor == key) {
+          return peeker;
+        }
+        peeker.Skip();
+      }
     }
   }
 }
