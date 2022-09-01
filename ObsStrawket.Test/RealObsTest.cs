5da4a5ea19279dea6101250d39bb1e83baaa4ef2
jeiea <solum5013@gmail.com>
2022-09-01 22:02:37 +0900

    test: skip real obs test in CI

--- ObsStrawket.Test/RealObsTest.cs
+++ ObsStrawket.Test/RealObsTest.cs
@@ -1,27 +1,38 @@
-namespace ObsStrawket.Test {
+namespace ObsStrawket.Test.Real {
   using ObsStrawket.Test.Utilities;
   using System;
   using System.Threading.Tasks;
   using Xunit;
-  using Xunit.Abstractions;
-
 
   public class RealObsTest {
+    private readonly bool _shouldSkip = false;
+
     public RealObsTest() {
+      _shouldSkip = Environment.GetEnvironmentVariable("CI") != null;
+      Console.WriteLine("");
     }
 
-    [Fact(Skip = "")]
+    [Fact()]
     public async Task TestNormalAsync() {
+      if (_shouldSkip) {
+        return;
+      }
       await new ClientFlow().RunClientAsync(new Uri("ws://127.0.0.1:4455")).ConfigureAwait(false);
     }
 
-    [Fact(Skip = "")]
+    [Fact()]
     public async Task TestBadRequestAsync() {
+      if (_shouldSkip) {
+        return;
+      }
       await ClientFlow.RequestBadAsync(new Uri("ws://127.0.0.1:4455")).ConfigureAwait(false);
     }
 
-    [Fact(Skip = "")]
+    [Fact()]
     public async Task JustMonitorObsEventAsync() {
+      if (_shouldSkip) {
+        return;
+      }
       var client = ClientFlow.GetDebugClient();
       await client.ConnectAsync(new Uri("ws://127.0.0.1:4455"), "ahrEYXzXKytCIlpI").ConfigureAwait(false);
       var taskSource = new TaskCompletionSource<object>();
