72e93b9d05883150d480c0fe7bf86ecc38397566
jeiea <solum5013@gmail.com>
2022-09-24 02:10:44 +0900

    fix: test failure

--- ObsStrawket/Serialization/OutputFlagsMapFormatter.cs
+++ ObsStrawket/Serialization/OutputFlagsMapFormatter.cs
@@ -4,10 +4,15 @@ using ObsStrawket.DataTypes;
 using System;
 
 namespace ObsStrawket.Serialization {
-  internal class OutputFlagsMapFormatter : IMessagePackFormatter<OutputFlags> {
+  /// <summary>
+  /// It is public due to library restriction and not intended for general use.
+  /// </summary>
+  public class OutputFlagsMapFormatter : IMessagePackFormatter<OutputFlags> {
     internal static readonly OutputFlagsMapFormatter Instance = new();
     private static readonly EnumAsStringFormatter<OutputFlags> _enumFormatter = new();
 
+    /// <summary>
+    /// </summary>
     public OutputFlags Deserialize(ref MessagePackReader reader, MessagePackSerializerOptions options) {
       OutputFlags value = 0;
 
@@ -22,6 +27,8 @@ namespace ObsStrawket.Serialization {
       return value;
     }
 
+    /// <summary>
+    /// </summary>
     public void Serialize(ref MessagePackWriter writer, OutputFlags value, MessagePackSerializerOptions options) {
       throw new NotImplementedException();
     }
