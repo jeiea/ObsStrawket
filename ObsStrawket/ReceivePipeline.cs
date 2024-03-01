df7ff0f882af7abf66763a60948d70b14d51235f
jeiea <solum5013@gmail.com>
2024-03-02 07:27:48 +0900

    stash: irritating

--- ObsStrawket/ReceivePipeline.cs
+++ ObsStrawket/ReceivePipeline.cs
@@ -1,7 +1,6 @@
 using Microsoft.Extensions.Logging;
 using ObsStrawket.DataTypes;
 using ObsStrawket.DataTypes.Predefineds;
-using ObsStrawket.Serialization;
 using System;
 using System.IO.Pipelines;
 using System.Net.WebSockets;
@@ -10,25 +9,30 @@ using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace ObsStrawket {
-  internal class ReceivePipeline {
-    private static readonly MessagePackSerializerOptions _serialOptions = new(OpCodeMessageResolver.Instance);
 
+  internal class ReceivePipeline {
     private readonly WebSocket _socket;
     private readonly ILogger? _logger;
     private readonly Channel<Task<IOpCodeMessage>> _messages = Channel.CreateUnbounded<Task<IOpCodeMessage>>();
     private readonly LazyString bytesBuilder = new();
-
-    public ChannelReader<Task<IOpCodeMessage>> Messages { get => _messages.Reader; }
-
-    public Task? ReceiveTask { get; private set; }
 
     public ReceivePipeline(WebSocket socket, ILogger? logger = null) {
       _socket = socket;
       _logger = logger;
     }
 
+    public ChannelReader<Task<IOpCodeMessage>> Messages { get => _messages.Reader; }
+
+    public Task? ReceiveTask { get; private set; }
+
     public void Run(CancellationToken token) {
       ReceiveTask ??= LoopWebSocketReceiveAsync(token: token);
+    }
+
+    private static async Task<IOpCodeMessage> DeserializeAsync(Pipe pipe, CancellationToken token = default) {
+      var buffer = await PipelineHelpers.RealAllAsync(pipe, token).ConfigureAwait(false);
+      var message = MessagePackSerializer.Deserialize<IOpCodeMessage>(buffer, _serialOptions, token);
+      return message;
     }
 
     private async Task LoopWebSocketReceiveAsync(int sizeHint = 0, CancellationToken token = default) {
@@ -54,6 +58,7 @@ namespace ObsStrawket {
             case (int?)WebSocketCloseCode.AuthenticationFailed:
               _messages.Writer.TryComplete(new AuthenticationFailureException());
               break;
+
             default:
               _messages.Writer.TryComplete(new WebsocketCloseReceivedException(code: (int?)_socket.CloseStatus));
               break;
@@ -79,18 +84,11 @@ namespace ObsStrawket {
         _messages.Writer.TryComplete(exception);
       }
     }
-
-    private static async Task<IOpCodeMessage> DeserializeAsync(Pipe pipe, CancellationToken token = default) {
-      var buffer = await PipelineHelpers.RealAllAsync(pipe, token).ConfigureAwait(false);
-      var message = MessagePackSerializer.Deserialize<IOpCodeMessage>(buffer, _serialOptions, token);
-      return message;
-    }
-
   }
 
   internal class LazyString {
-    Func<string> _builder = () => "";
-    string? _evaluated;
+    private Func<string> _builder = () => "";
+    private string? _evaluated;
 
     public Func<string> Builder {
       get => _builder;
@@ -105,4 +103,3 @@ namespace ObsStrawket {
     }
   }
 }
-
