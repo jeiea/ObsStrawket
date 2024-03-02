485363d23fd218468f2bc08c246554ad74d47bcc
jeiea <solum5013@gmail.com>
2024-03-03 04:03:18 +0900

    feat: refresh data type mapping

--- ObsStrawket/DataTypes/DataTypeMapping.cs
+++ ObsStrawket/DataTypes/DataTypeMapping.cs
@@ -6,12 +6,13 @@ using System.Linq;
 namespace ObsStrawket.DataTypes {
   internal static class DataTypeMapping {
     internal static readonly Dictionary<string, Type> EventToTypes = new Type[] {
-      // General Events,
-      typeof(ExitStarted),
-      typeof(CustomEvent),
-      typeof(VendorEvent),
 
-      // Config Events,
+      // General Events
+      typeof(ExitStarted),
+      typeof(VendorEvent),
+      typeof(CustomEvent),
+
+      // Config Events
       typeof(CurrentSceneCollectionChanging),
       typeof(CurrentSceneCollectionChanged),
       typeof(SceneCollectionListChanged),
@@ -19,7 +20,7 @@ namespace ObsStrawket.DataTypes {
       typeof(CurrentProfileChanged),
       typeof(ProfileListChanged),
 
-      // Scenes Events,
+      // Scenes Events
       typeof(SceneCreated),
       typeof(SceneRemoved),
       typeof(SceneNameChanged),
@@ -27,10 +28,11 @@ namespace ObsStrawket.DataTypes {
       typeof(CurrentPreviewSceneChanged),
       typeof(SceneListChanged),
 
-      // Inputs Events,
+      // Inputs Events
       typeof(InputCreated),
       typeof(InputRemoved),
       typeof(InputNameChanged),
+      typeof(InputSettingsChanged),
       typeof(InputActiveStateChanged),
       typeof(InputShowStateChanged),
       typeof(InputMuteStateChanged),
@@ -41,21 +43,22 @@ namespace ObsStrawket.DataTypes {
       typeof(InputAudioMonitorTypeChanged),
       typeof(InputVolumeMeters),
 
-      // Transitions Events,
+      // Transitions Events
       typeof(CurrentSceneTransitionChanged),
       typeof(CurrentSceneTransitionDurationChanged),
       typeof(SceneTransitionStarted),
       typeof(SceneTransitionEnded),
       typeof(SceneTransitionVideoEnded),
 
-      // Filters Events,
+      // Filters Events
       typeof(SourceFilterListReindexed),
       typeof(SourceFilterCreated),
       typeof(SourceFilterRemoved),
       typeof(SourceFilterNameChanged),
+      typeof(SourceFilterSettingsChanged),
       typeof(SourceFilterEnableStateChanged),
 
-      // Scene Items Events,
+      // SceneItems Events
       typeof(SceneItemCreated),
       typeof(SceneItemRemoved),
       typeof(SceneItemListReindexed),
@@ -64,26 +67,27 @@ namespace ObsStrawket.DataTypes {
       typeof(SceneItemSelected),
       typeof(SceneItemTransformChanged),
 
-      // Outputs Events,
+      // Outputs Events
       typeof(StreamStateChanged),
       typeof(RecordStateChanged),
       typeof(ReplayBufferStateChanged),
       typeof(VirtualcamStateChanged),
       typeof(ReplayBufferSaved),
 
-      // Media Inputs Events,
+      // MediaInputs Events
       typeof(MediaInputPlaybackStarted),
       typeof(MediaInputPlaybackEnded),
       typeof(MediaInputActionTriggered),
 
-      // Ui Events,
+      // Ui Events
       typeof(StudioModeStateChanged),
       typeof(ScreenshotSaved),
-    }.ToDictionary(t => t.Name, t => t);
+    }.ToDictionary(x => x.Name, x => x);
 
-    internal record class RequestMapping(Type Request, Type Response, bool IsRequestEmpty = false);
+    internal record RequestMapping(Type Request, Type Response, bool IsRequestEmpty = false);
 
     internal static readonly Dictionary<string, RequestMapping> RequestToTypes = new RequestMapping[] {
+
       // General Requests
       new (typeof(GetVersion), typeof(GetVersionResponse), true),
       new (typeof(GetStats), typeof(GetStatsResponse), true),
@@ -93,6 +97,7 @@ namespace ObsStrawket.DataTypes {
       new (typeof(TriggerHotkeyByName), typeof(RequestResponse)),
       new (typeof(TriggerHotkeyByKeySequence), typeof(RequestResponse)),
       new (typeof(Sleep), typeof(RequestResponse)),
+
       // Config Requests
       new (typeof(GetPersistentData), typeof(GetPersistentDataResponse)),
       new (typeof(SetPersistentData), typeof(RequestResponse)),
@@ -110,10 +115,13 @@ namespace ObsStrawket.DataTypes {
       new (typeof(GetStreamServiceSettings), typeof(GetStreamServiceSettingsResponse), true),
       new (typeof(SetStreamServiceSettings), typeof(RequestResponse)),
       new (typeof(GetRecordDirectory), typeof(GetRecordDirectoryResponse), true),
+      new (typeof(SetRecordDirectory), typeof(RequestResponse)),
+
       // Sources Requests
       new (typeof(GetSourceActive), typeof(GetSourceActiveResponse)),
       new (typeof(GetSourceScreenshot), typeof(GetSourceScreenshotResponse)),
       new (typeof(SaveSourceScreenshot), typeof(RequestResponse)),
+
       // Scenes Requests
       new (typeof(GetSceneList), typeof(GetSceneListResponse), true),
       new (typeof(GetGroupList), typeof(GetGroupListResponse), true),
@@ -121,11 +129,12 @@ namespace ObsStrawket.DataTypes {
       new (typeof(SetCurrentProgramScene), typeof(RequestResponse)),
       new (typeof(GetCurrentPreviewScene), typeof(GetCurrentPreviewSceneResponse), true),
       new (typeof(SetCurrentPreviewScene), typeof(RequestResponse)),
-      new (typeof(CreateScene), typeof(RequestResponse)),
+      new (typeof(CreateScene), typeof(CreateSceneResponse)),
       new (typeof(RemoveScene), typeof(RequestResponse)),
       new (typeof(SetSceneName), typeof(RequestResponse)),
       new (typeof(GetSceneSceneTransitionOverride), typeof(GetSceneSceneTransitionOverrideResponse)),
       new (typeof(SetSceneSceneTransitionOverride), typeof(RequestResponse)),
+
       // Inputs Requests
       new (typeof(GetInputList), typeof(GetInputListResponse)),
       new (typeof(GetInputKindList), typeof(GetInputKindListResponse)),
@@ -151,6 +160,7 @@ namespace ObsStrawket.DataTypes {
       new (typeof(SetInputAudioTracks), typeof(RequestResponse)),
       new (typeof(GetInputPropertiesListPropertyItems), typeof(GetInputPropertiesListPropertyItemsResponse)),
       new (typeof(PressInputPropertiesButton), typeof(RequestResponse)),
+
       // Transitions Requests
       new (typeof(GetTransitionKindList), typeof(GetTransitionKindListResponse), true),
       new (typeof(GetSceneTransitionList), typeof(GetSceneTransitionListResponse), true),
@@ -161,7 +171,9 @@ namespace ObsStrawket.DataTypes {
       new (typeof(GetCurrentSceneTransitionCursor), typeof(GetCurrentSceneTransitionCursorResponse), true),
       new (typeof(TriggerStudioModeTransition), typeof(RequestResponse), true),
       new (typeof(SetTBarPosition), typeof(RequestResponse)),
+
       // Filters Requests
+      new (typeof(GetSourceFilterKindList), typeof(GetSourceFilterKindListResponse), true),
       new (typeof(GetSourceFilterList), typeof(GetSourceFilterListResponse)),
       new (typeof(GetSourceFilterDefaultSettings), typeof(GetSourceFilterDefaultSettingsResponse)),
       new (typeof(CreateSourceFilter), typeof(RequestResponse)),
@@ -171,10 +183,12 @@ namespace ObsStrawket.DataTypes {
       new (typeof(SetSourceFilterIndex), typeof(RequestResponse)),
       new (typeof(SetSourceFilterSettings), typeof(RequestResponse)),
       new (typeof(SetSourceFilterEnabled), typeof(RequestResponse)),
-      // Scene Items Requests
+
+      // SceneItems Requests
       new (typeof(GetSceneItemList), typeof(GetSceneItemListResponse)),
       new (typeof(GetGroupSceneItemList), typeof(GetGroupSceneItemListResponse)),
       new (typeof(GetSceneItemId), typeof(GetSceneItemIdResponse)),
+      new (typeof(GetSceneItemSource), typeof(GetSceneItemSourceResponse)),
       new (typeof(CreateSceneItem), typeof(CreateSceneItemResponse)),
       new (typeof(RemoveSceneItem), typeof(RequestResponse)),
       new (typeof(DuplicateSceneItem), typeof(DuplicateSceneItemResponse)),
@@ -188,6 +202,7 @@ namespace ObsStrawket.DataTypes {
       new (typeof(SetSceneItemIndex), typeof(RequestResponse)),
       new (typeof(GetSceneItemBlendMode), typeof(GetSceneItemBlendModeResponse)),
       new (typeof(SetSceneItemBlendMode), typeof(RequestResponse)),
+
       // Outputs Requests
       new (typeof(GetVirtualCamStatus), typeof(GetVirtualCamStatusResponse), true),
       new (typeof(ToggleVirtualCam), typeof(ToggleVirtualCamResponse), true),
@@ -206,12 +221,14 @@ namespace ObsStrawket.DataTypes {
       new (typeof(StopOutput), typeof(RequestResponse)),
       new (typeof(GetOutputSettings), typeof(GetOutputSettingsResponse)),
       new (typeof(SetOutputSettings), typeof(RequestResponse)),
+
       // Stream Requests
       new (typeof(GetStreamStatus), typeof(GetStreamStatusResponse), true),
       new (typeof(ToggleStream), typeof(ToggleStreamResponse), true),
       new (typeof(StartStream), typeof(RequestResponse), true),
       new (typeof(StopStream), typeof(RequestResponse), true),
       new (typeof(SendStreamCaption), typeof(RequestResponse)),
+
       // Record Requests
       new (typeof(GetRecordStatus), typeof(GetRecordStatusResponse), true),
       new (typeof(ToggleRecord), typeof(ToggleRecordResponse), true),
@@ -220,11 +237,13 @@ namespace ObsStrawket.DataTypes {
       new (typeof(ToggleRecordPause), typeof(ToggleRecordPauseResponse), true),
       new (typeof(PauseRecord), typeof(RequestResponse), true),
       new (typeof(ResumeRecord), typeof(RequestResponse), true),
-      // Media Inputs Requests
+
+      // MediaInputs Requests
       new (typeof(GetMediaInputStatus), typeof(GetMediaInputStatusResponse)),
       new (typeof(SetMediaInputCursor), typeof(RequestResponse)),
       new (typeof(OffsetMediaInputCursor), typeof(RequestResponse)),
       new (typeof(TriggerMediaInputAction), typeof(RequestResponse)),
+
       // Ui Requests
       new (typeof(GetStudioModeEnabled), typeof(GetStudioModeEnabledResponse), true),
       new (typeof(SetStudioModeEnabled), typeof(RequestResponse)),
@@ -234,7 +253,6 @@ namespace ObsStrawket.DataTypes {
       new (typeof(GetMonitorList), typeof(GetMonitorListResponse), true),
       new (typeof(OpenVideoMixProjector), typeof(RequestResponse)),
       new (typeof(OpenSourceProjector), typeof(RequestResponse)),
-    }.ToDictionary(t => t.Request.Name, t => t);
-
+    }.ToDictionary(x => x.Request.Name, x => x);
   }
 }
